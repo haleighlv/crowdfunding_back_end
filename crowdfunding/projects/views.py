3ed3bceef10933c4c6c1f1a9408bbbc7092caca9
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-09-21 11:01:07 +1000

    views

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -22,3 +23,17 @@ class ProjectList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectDetail(APIView):
+    def get_object(self, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+            return project
+        except Project.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
